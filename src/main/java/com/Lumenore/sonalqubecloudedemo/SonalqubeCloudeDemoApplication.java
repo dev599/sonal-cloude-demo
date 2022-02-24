87bc081cdca04e68d920ba8d1039f9106a6aeed5
Devendra Yadav <DYadav@bpl.netlink.com>
2022-02-24 12:57:53 +0530

    initial new one

---
+++ src/main/java/com/Lumenore/sonalqubecloudedemo/SonalqubeCloudeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Lumenore.sonalqubecloudedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonalqubeCloudeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonalqubeCloudeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lumenore/sonalqubecloudedemo/SonalqubeCloudeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lumenore.sonalqubecloudedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonalqubeCloudeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
