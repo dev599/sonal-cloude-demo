fe730486f926343c0d7c2291436e88318e5908e1
Devendra Yadav <DYadav@bpl.netlink.com>
2022-02-25 15:44:33 +0530

    initial new update application

---
+++ src/main/java/rest/DemoRestController.java
@@ -0,0 +1,18 @@
+package rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+	
+	@RequestMapping(value="/api/capitalize")
+	public ResponseEntity<String> capitalize(@RequestParam("test") String text){
+		
+		String capital = text.toUpperCase();
+		return ResponseEntity.ok(capital);
+		
+	}
+}
